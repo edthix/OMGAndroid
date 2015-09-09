7b5cd504a6ee83c6ed0389068ff42dbb8bda460c
Edham Arief / edthix <edthix@gmail.com>
2015-09-09 23:04:26 +0800

    Part 3 - Putting Together the Insta-Row

--- app/src/main/java/com/example/omgandroid/JSONAdapter.java
+++ app/src/main/java/com/example/omgandroid/JSONAdapter.java
@@ -5,8 +5,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import org.json.JSONArray;
+import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 /**
  * Created by ed on 9/9/15.
@@ -43,6 +49,75 @@ public class JSONAdapter extends BaseAdapter{
 
     @Override
     public View getView(int i, View view, ViewGroup viewGroup) {
-        return null;
+        ViewHolder holder;
+
+        // Check if item already exists
+        // if so, no need to inflate and findViewById again!
+        if (view == null) {
+
+            // Inflate the custom row layout from your XML
+            view = mInflater.inflate(R.layout.row_book, null);
+
+            // create a new "Holder" with subviews
+            holder = new ViewHolder();
+            holder.thumbnailImageView = (ImageView) view.findViewById(R.id.img_thumbnail);
+            holder.titleTextView = (TextView) view.findViewById(R.id.text_title);
+            holder.authorTextView = (TextView) view.findViewById(R.id.text_author);
+
+            // hang onto this holder for future recycle
+            view.setTag(holder);
+
+        } else {
+            // skip all the expensive inflation/findViewById
+            // and just get the holder you already made
+            holder = (ViewHolder) view.getTag();
+        }
+
+        // Get the current book's data in JSON form
+        JSONObject jsonObject = (JSONObject) getItem(i);
+
+        // See if there is a cover id in the object
+        if(jsonObject.has("cover_i")) {
+            // If so, grab the cover id out from the object
+            String imageID = jsonObject.optString("cover_i");
+
+            // Construct the image URL (specific to API)
+            String imageURL = IMAGE_URL_BASE + imageID + "-S.jpg";
+
+            // Use Picasso to load the image
+            // Temporarily have a placeholder in case it's slow to load
+
+            Picasso.with(mContext).load(imageURL).placeholder(R.drawable.ic_books).into(holder
+                    .thumbnailImageView);
+
+        } else {
+            // If there is no cover ID in the object, use a placeholder
+            holder.thumbnailImageView.setImageResource(R.drawable.ic_books);
+        }
+
+        // Grab the title and author from the JSON
+        String bookTitle = "";
+        String authorName = "";
+
+        if (jsonObject.has("title")) {
+            bookTitle = jsonObject.optString("title");
+        }
+
+        if (jsonObject.has("author_name")) {
+            authorName = jsonObject.optJSONArray("author_name").optString(0);
+        }
+
+        // Send these Strings to the TextViews for display
+        holder.titleTextView.setText(bookTitle);
+        holder.authorTextView.setText(authorName);
+
+        return view;
+    }
+
+    // This is used so you only ever have to do inflation and finding by ID once ever per view
+    private static class ViewHolder {
+        public ImageView thumbnailImageView;
+        public TextView titleTextView;
+        public TextView authorTextView;
     }
 }
