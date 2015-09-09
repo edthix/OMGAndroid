fafe48b8e0367d4ac1c04004c3c05e8af9f4c196
Edham Arief / edthix <edthix@gmail.com>
2015-09-10 00:24:02 +0800

    Part 3 - An Intent to Show the Detail Activity

--- app/src/main/java/com/example/omgandroid/DetailActivity.java
+++ app/src/main/java/com/example/omgandroid/DetailActivity.java
@@ -1,15 +1,21 @@
 package com.example.omgandroid;
 
+import android.app.ActionBar;
 import android.app.Activity;
 import android.media.Image;
 import android.os.Bundle;
 import android.widget.ImageView;
 
+import com.squareup.picasso.Picasso;
+
 /**
  * Created by ed on 9/9/15.
  */
 public class DetailActivity extends Activity {
-    @Override
+    private static final String IMAGE_URL_BASE = "http://covers.openlibrary.org/b/id/"; // 13
+    String mImageURL; // 13
+
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
@@ -17,9 +23,26 @@ public class DetailActivity extends Activity {
         setContentView(R.layout.activity_detail);
 
         // Enable the "Up" button for more navigation options
-        getActionBar().setDisplayHomeAsUpEnabled(true);
-
+        //getActionBar().setDisplayHomeAsUpEnabled(true);
+       
         // Access the imageview from XML
         ImageView imageView = (ImageView) findViewById(R.id.img_cover);
+
+        // 13. unpack the coverID from its trip inside your Intent
+        String coverID =
+                this.getIntent().getExtras().getString("coverID");
+
+        // See if there is a valid coverID
+        if (coverID.length() > 0) {
+
+            // Use the ID to construct an image URL
+            mImageURL = IMAGE_URL_BASE + coverID + "-L.jpg";
+
+            // Use Picasso to load the image
+            Picasso.with(this)
+                    .load(mImageURL)
+                    .placeholder(R.drawable.img_books_loading)
+                    .into(imageView);
+        }
     }
 }
